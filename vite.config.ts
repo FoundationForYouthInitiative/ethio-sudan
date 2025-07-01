1d30ca3d9a51591c59ada4d92b22cd52f833b538
FoundationForYouthInitiative <foundationforyouthinitiative0@gmail.com>
2025-07-01 18:02:35 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? 'https://github.com/FoundationForYouthInitiative/ethio-sudan/blob/main/vite.config.ts' : '/',
+  base: command === 'build' ? '/ethio-sudan/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
